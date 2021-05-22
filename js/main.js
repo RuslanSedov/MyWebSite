3a65a7f2242213d235abfc587f21341db3b78a6e
RuslanSedov <asjr2727@yandex.ru>
2021-05-22 18:55:20 +0300

    Modified start-page

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let button = document.querySelector('#list-button')
+let list = document.querySelector('#main-list')
+let width = document.documentElement.clientWidth
+button.addEventListener('click', () => {
+  list.classList.toggle('menu__list-hidden')
+})
+if (width <= 650) {
+  list.classList.toggle('menu__list-hidden')
+}

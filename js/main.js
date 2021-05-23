4fc277a2011ad69c3e4ae2fc727bc24b8e50aef3
RuslanSedov <asjr2727@yandex.ru>
2021-05-23 13:24:09 +0300

    Modified menu-transform

--- js/main.js
+++ js/main.js
@@ -2,7 +2,17 @@ let button = document.querySelector('#list-button')
 let list = document.querySelector('#main-list')
 let width = document.documentElement.clientWidth
 button.addEventListener('click', () => {
-  list.classList.toggle('menu__list-hidden')
+  if (list.classList.contains('menu__list-close')) {
+    button.classList.remove('menu__button-close')
+    button.classList.add('menu__button-open')
+    list.classList.remove('menu__list-close')
+    list.classList.add('menu__list-open')
+  } else {
+    button.classList.remove('menu__button-open')
+    button.classList.add('menu__button-close')
+    list.classList.remove('menu__list-open')
+    list.classList.add('menu__list-close')
+  }
 })
 // if (width <= 650) {
 //   list.classList.toggle('menu__list-hidden')
